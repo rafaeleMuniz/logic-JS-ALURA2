cb0b73a010dfde43c7a1f748383af2fedf978d04
Rafaele Muniz <rafaeleschmoeller@gmail.com>
2024-01-06 21:52:17 -0300

    Continuando o curso - bloco 4

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let = numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1; 
 
@@ -40,7 +42,18 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10) + 1 //return para a variável ser armazenada na l1
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite) + 1;
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio(); //se o número já foi escolhido, gera um novo
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo(){

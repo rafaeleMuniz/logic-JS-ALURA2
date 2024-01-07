d6a49a0abf5a903ed3c526cdb707aed1772c5fd5
Rafaele Muniz <rafaeleschmoeller@gmail.com>
2024-01-06 21:02:12 -0300

    continuando o curso

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1; 
 
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo do número secreto';
@@ -11,16 +12,47 @@ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-//chamando a função pós criação
-exibirTextoNaTela('h1','Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+exibirMensagemInicial();
 
 
 function verificarChute(){
     let chute = document.querySelector('input').value
-    console.log(chute == numeroSecreto); //boolean - true or false 
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Você acertou!');
+        let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar'). removeAttribute('disabled');//precisa ser igual o HTML
+    }else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número é menor');
+        }else{
+            exibirTextoNaTela('p', 'O número é maior');
+        }
+        tentativas++
+        limparCampo();
+    }
+    
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10) + 1 //return para a variável ser armazenada na l1
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = ''; //seja uma string vazia
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+
 }
